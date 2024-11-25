bd6f075d851c9315b18c22854af831803603a64c
Neto test <danya2007jason@gmail.com>
2024-11-25 21:37:34 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,102 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course].append(grade)
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def average_grade(self):
+        total_grades = sum(sum(grades) for grades in self.grades.values())
+        total_courses = sum(len(grades) for grades in self.grades.values())
+        return total_grades / total_courses if total_courses > 0 else 0
+
+    def __str__(self):
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за домашние задания: {self.average_grade():.2f}\n'
+                f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
+                f'Завершенные курсы: {", ".join(self.finished_courses)}')
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course].append(grade)
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        return f'Имя: {self.name}\nФамилия: {self.surname}'
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+    def average_grade(self):
+        total_grades = sum(sum(grades) for grades in self.grades.values())
+        total_courses = sum(len(grades) for grades in self.grades.values())
+        return total_grades / total_courses if total_courses > 0 else 0
+
+    def __str__(self):
+        return (f'Имя: {self.name}\n'
+                f'Фамилия: {self.surname}\n'
+                f'Средняя оценка за лекции: {self.average_grade():.2f}')
+
+    def __lt__(self, other):
+        if isinstance(other, Lecturer):
+            return self.average_grade() < other.average_grade()
+        return NotImplemented
+
+
+# Functions for calculating average grades
+def average_student_grade(students, course):
+    total_grades = 0
+    count = 0
+    for student in students:
+        if course in student.grades:
+            total_grades += sum(student.grades[course])
+            count += len(student.grades[course])
+    return total_grades / count if count > 0 else 0
+
+
+def average_lecturer_grade(lecturers, course):
+    total_grades = 0
+    count = 0
+    for lecturer in lecturers:
+        if course in lecturer.grades:
+            total_grades += sum(lecturer.grades[course])
+            count += len(lecturer.grades[course])
+    return total_grades / count if count > 0 else 0
+
+
+# Example usage
+student1 = Student('Ruoy', 'Eman', 'your_gender')
+student1.courses_in_progress.append('Python')
+student1.courses_in_progress.append('Git')
+
+student2 = Student('John', 'Doe', 'your_gender')
+student2.courses_in_progress
